c37e61553e64bff78e213a70baa75878c16f515e
abdulrahmanahmedgalhoom-pixel <abdulrahmanahmedgalhoom@gmail.com>
2026-07-23 12:15:48 +0300

    Create Document.js

---
+++ Document.js
@@ -0,0 +1,28 @@
+// ============================
+// Document Actions
+// ============================
+
+// حذف كل المحتوى
+
+if(deleteAll){
+
+    deleteAll.addEventListener("click", function(){
+
+        editor.innerHTML = "";
+
+    });
+
+}
+
+
+// إنشاء ملف جديد
+
+if(newBtn){
+
+    newBtn.addEventListener("click", function(){
+
+        editor.innerHTML = "";
+
+    });
+
+}
